a59e5084fcdd38f144bf2cfac9a883fc8e4e60c5
swapnil <swapnilhgf@gmail.com>
2023-08-26 22:07:05 +0530

    edit payload

--- components/tabBody/apiGenerator/layout.js
+++ components/tabBody/apiGenerator/layout.js
@@ -120,14 +120,46 @@ const MyCompProfile = params => {
 
   const payloadCell = (params)=>{
    
+    const [rowIndex, setRowIndex] = useState(null);
+    const handleDelete = (index) => {
+        const updatedPayload = params.data.payload.filter((_, i) => i !== index);
+        params.node.setDataValue('payload', updatedPayload);
+    };
    
-    if(true){
-        return (<div class={`grid ${"grid-rows-"+(params.data.payload.length>2?'3':params.data.payload.length<2?'1':'2')} justify-start grid-flow-col gap-y-2 py-2`}>
+    if(params.rowIndex != rowIndex ){
+        if(params.data.payload.length == 0){
+            return(
+                <div className=" float-left pb-5 pt-2 ">
+              <div className="grid grid-flow-row gap-1" >
+                  <div className=" flex justify-between items-center gap-2">
+                            <div className="flex gap-1">
+                            <input type="text" class="h-8 w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Field Name" required/>
+                            <input type="text" class="h-8 w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Data Type" required/>
+                            </div> 
+                            <div className=" ">
+                
+                                <svg className="text-red-400 hover:bg-white hover:cursor-pointer rounded-full w-5 h-5 font-block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                </svg>
+
+                            </div>
+                        </div>
+              </div>
+              <div className=" float-right " title="Add Input Field">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="text-[#F0BD66] hover:bg-white hover:cursor-pointer rounded-full w-5 h-5 font-block" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                </svg>
+              </div>
+              
+            </div>
+            )
+        }else{
+            return (<div class={`grid ${"grid-rows-"+(params.data.payload.length>2?'3':params.data.payload.length<2?'1':'2')} justify-start grid-flow-col gap-y-2 py-2`}>
         {params.data.payload.map((item, index) => {
             return (
                 <div class="flex-shrink-0 inline-flex max-w-fit items-center px-2 py-1 mr-2 text-xs text-gray-800 bg-[#F0BD66] rounded">
-                    {item.fieldName} - {item.dataType}
-                    <button type="button" class="inline-flex items-center p-1 ml-2 text-sm text-gray-400 bg-transparent rounded-sm hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-gray-300" data-dismiss-target="#badge-dismiss-dark" aria-label="Remove">
+                    <span onClick={()=>{setRowIndex(params.rowIndex)}} className=" hover:cursor-pointer">{item.fieldName} - {item.dataType}</span>
+                    <button onClick={()=>{handleDelete(index)}} type="button" class="inline-flex items-center p-1 ml-2 text-sm text-gray-400 bg-transparent rounded-sm hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-gray-300" data-dismiss-target="#badge-dismiss-dark" aria-label="Remove">
                         <svg class="w-2 h-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                         </svg>
@@ -137,9 +169,11 @@ const MyCompProfile = params => {
             );
         })}
     </div>)
+        }
+        
     }else{
         return(<>
-            <div className=" absolute left-0 pt-2 ">
+            <div className=" float-left pb-5 pt-2 ">
               <div className="grid grid-flow-row gap-1" >
                   {params.data.payload.map((item,index)=>{
                     return (<div className=" flex justify-between items-center gap-2">
@@ -147,7 +181,7 @@ const MyCompProfile = params => {
                             <input type="text" class="h-8 w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Field Name" required/>
                             <input type="text" class="h-8 w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Data Type" required/>
                             </div> 
-                            <div className=" ">
+                            <div onClick={()=>{handleDelete(index)}} className=" ">
                 
                                 <svg className="text-red-400 hover:bg-white hover:cursor-pointer rounded-full w-5 h-5 font-block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
                                      <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
@@ -163,7 +197,7 @@ const MyCompProfile = params => {
                                 <svg xmlns="http://www.w3.org/2000/svg" className="text-[#F0BD66] hover:bg-white hover:cursor-pointer rounded-full w-5 h-5 font-block" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                                 </svg>
-                            </div>
+              </div>
               
             </div>
             </>)
@@ -179,7 +213,7 @@ const MyCompProfile = params => {
         {params.data.statusCode.map((item, index) => {
             return (
                 <div class="flex-shrink-0 inline-flex max-w-fit items-center px-2 py-1 mr-2 text-xs text-gray-800 bg-slate-300 rounded">
-                    {item.code} - {item.remark}
+                    <span className=" hover:cursor-pointer">{item.code} - {item.remark}</span>
                     <button type="button" class="inline-flex items-center p-1 ml-2 text-sm text-gray-400 bg-transparent rounded-sm hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-gray-300" data-dismiss-target="#badge-dismiss-dark" aria-label="Remove">
                         <svg class="w-2 h-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
@@ -247,7 +281,7 @@ const Layout = (props) => {
             "resource":"Register User",
             "address":"/register",
             "protocol":"POST",
-            "payload":[{"fieldName":"username","dataType":"string"}],
+            "payload":[],
             "statusCode":[{"code":"200","remark":"OK"},{"code":"300","remark":"user not found"}],
             
         },{
@@ -339,24 +373,37 @@ const Layout = (props) => {
             };
           };
 
-          const getRowHeight = useCallback((params) => {
-           const rowHeight = 0
-            switch (3   ) {
-                case 0:
-                    rowHeight = params.data.payload.length*35 + 45
-                    break;
-                case 1:
-                    rowHeight = params.data.statusCode.length*35 + 45
-                    break;
-                case 2:
-                    rowHeight = Math.max(...[params.data.payload.length*35 + 45, params.data.statusCode.length*35 + 45])
-                    break;
+        //   const getRowHeight = useCallback((params) => {
+        //    const rowHeight = 0
+        //     switch (3   ) {
+        //         case 0:
+        //             rowHeight = params.data.payload.length*35 + 45
+        //             break;
+        //         case 1:
+        //             rowHeight = params.data.statusCode.length*35 + 45
+        //             break;
+        //         case 2:
+        //             rowHeight = Math.max(...[params.data.payload.length*35 + 45, params.data.statusCode.length*35 + 45])
+        //             break;
             
-                default:
-                    rowHeight = 45
-                    break;
+        //         default:
+        //             rowHeight = 45
+        //             break;
+        //     }
+        //     return  rowHeight
+        //   }, []);
+
+          const onCellValueChanged = useCallback((params) => {
+            const colId = params.column.getId();
+            if (colId === 'country') {
+              const selectedCountry = params.data.country;
+              const selectedCity = params.data.city;
+              const allowedCities = countyToCityMap(selectedCountry);
+              const cityMismatch = allowedCities.indexOf(selectedCity) < 0;
+              if (cityMismatch) {
+                params.node.setDataValue('city', null);
+              }
             }
-            return  rowHeight
           }, []);
 
           const containerStyle = useMemo(() => ({
@@ -429,8 +476,10 @@ const Layout = (props) => {
                                             getRowStyle={getRowStyle}
 
                                             defaultColDef={defaultColDef}
-                                            getRowHeight={getRowHeight}
+                                            // getRowHeight={getRowHeight}
                                             pagination={true}
+                                            onCellValueChanged={onCellValueChanged}
+
                                             // rowSelection={'single'}
                                             // onSelectionChanged={onSelectionChanged}
 
